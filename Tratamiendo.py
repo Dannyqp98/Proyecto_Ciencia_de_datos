8e6594597bcd3a296f870f63c1149f80f9376e46
jdbuilesm <jdbuilesm@unal.edu.co>
2023-03-27 13:31:45 -0500

    Eliminación index

--- Tratamiendo.py
+++ Tratamiendo.py
@@ -1,6 +1,7 @@
 import gpxpy
 import pandas as pd
 from geopy.geocoders import Nominatim
+import sqlite3
 
 geolocator = Nominatim(user_agent="geoapiExercises")
 
@@ -44,6 +45,10 @@ df1 = read_gpx('recovery.01-Mar-2022-1533.gpx')
 df2 = read_gpx('recovery.05-Mar-2022.1025.gpx')
 df3 = read_gpx('recovery.25-May-2022-0907.gpx')
 
+df1 = df1.round({'latitude': 6, 'longitude': 6, 'elevation': 2})
+df2 = df2.round({'latitude': 6, 'longitude': 6, 'elevation': 2})
+df3 = df3.round({'latitude': 6, 'longitude': 6, 'elevation': 2})
+
 dft=pd.concat([df1,df2,df3],axis=0).reset_index()
 dft['time']=dft.time.astype(str)
 dft['time']=pd.to_datetime(dft['time'])
@@ -77,6 +82,46 @@ for col in cols_merge:
   for char in replace_chars:
     df_export[col]=df_export[col].str.replace(char[0],char[1])
 
+df_export['time']=df_export['time'].values.astype('datetime64[s]')
+# df_export
+# df_export.to_csv('Coordenadas.csv',sep=';',index=False)
 
-df_export
-df_export.to_csv('Coordenadas.csv',sep=';',index=False)
+# Base de datos 
+df_export.drop(['index'], axis=1)
+df_export['time']=df_export['time'].values.astype('datetime64[s]')
+
+conn = sqlite3.connect('base_general.db')
+conn.execute('''CREATE TABLE datos_crudos (
+              level_0 INT,
+              time TEXT, 
+              latitude FLOAT, 
+              longitude FLOAT, 
+              elevation FLOAT, 
+              Round_lat FLOAT,
+              Round_long FLOAT,
+              barrio TEXT,
+              city TEXT,
+              state TEXT,
+              country TEXT
+
+)''')
+
+# level_0;index;time;latitude;longitude;elevation;Round_lat;Round_long;barrio;city;state;country
+
+for index, row in df_export.iterrows():
+    query = "INSERT INTO datos_crudos (level_0, time, latitude, longitude, elevation, Round_lat, Round_long, barrio, city, state, country ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    conn.execute(query, (
+                        row['level_0'], 
+                        row['time'].strftime('%Y-%m-%d %H:%M:%S'),
+                        row['latitude'], 
+                        row['longitude'], 
+                        row['elevation'], 
+                        row['Round_lat'],
+                        row['Round_long'],
+                        row['barrio'], 
+                        row['city'],
+                        row['state'], 
+                        row['country']))
+
+conn.commit()
+conn.close()
