826600b158287409ff30cb4995289f78ca281bae
Carlos Cuastumal <carloscuastumal64@gmail.com>
2023-03-27 15:36:32 -0500

    Se crea tablas de datos crudos y calculados

--- Tratamiendo.py
+++ Tratamiendo.py
@@ -29,26 +29,24 @@ def city_state_country(row):
     location = geolocator.reverse(coord, exactly_one=True)
     address = location.raw['address']
     barrio=address.get('neighbourhood','')
-    #suburb=address.get('suburb','')
     city = address.get('city', '')
     state = address.get('state', '')
     country = address.get('country', '')
     row['barrio'] = barrio
-    #row['sub_urb'] = suburb
     row['city'] = city
     row['state'] = state
     row['country'] = country
     return row
 
+df11 = read_gpx('recovery.01-Mar-2022-1533.gpx')
+df22 = read_gpx('recovery.05-Mar-2022.1025.gpx')
+df33 = read_gpx('recovery.25-May-2022-0907.gpx')
 
-df1 = read_gpx('recovery.01-Mar-2022-1533.gpx')
-df2 = read_gpx('recovery.05-Mar-2022.1025.gpx')
-df3 = read_gpx('recovery.25-May-2022-0907.gpx')
+df1 = df11.round({'latitude': 6, 'longitude': 6, 'elevation': 2})
+df2 = df22.round({'latitude': 6, 'longitude': 6, 'elevation': 2})
+df3 = df33.round({'latitude': 6, 'longitude': 6, 'elevation': 2})
 
-df1 = df1.round({'latitude': 6, 'longitude': 6, 'elevation': 2})
-df2 = df2.round({'latitude': 6, 'longitude': 6, 'elevation': 2})
-df3 = df3.round({'latitude': 6, 'longitude': 6, 'elevation': 2})
-
+dftc=pd.concat([df11,df22,df33],axis=0).reset_index()
 dft=pd.concat([df1,df2,df3],axis=0).reset_index()
 dft['time']=dft.time.astype(str)
 dft['time']=pd.to_datetime(dft['time'])
@@ -61,7 +59,6 @@ df_locs = df_group.apply(city_state_country, axis=1)
 df_locs.reset_index()
 cols_locs=['time', 'latitude', 'longitude', 'elevation', 'Round_lat',
        'Round_long', 'barrio', 'city', 'state', 'country']
-
 df_locs=df_locs[cols_locs]
 
 cols_merge=[col  for col in df_locs.columns if col not in dft.columns]
@@ -70,11 +67,8 @@ for key in keys:
   cols_merge.append(key)
 
 df_locations_merge=df_locs[cols_merge]
-
 df_export=dft.merge(df_locations_merge,how='left',on=['Round_lat','Round_long']).reset_index()
-
 df_export[cols_merge]=df_export[cols_merge].replace('','Sin data')
-
 replace_chars=[('á','a'),('é','e'),('í','i'),('ó','o'),('ú','u')]
 
 for col in cols_merge:
@@ -83,15 +77,33 @@ for col in cols_merge:
     df_export[col]=df_export[col].str.replace(char[0],char[1])
 
 df_export['time']=df_export['time'].values.astype('datetime64[s]')
-# df_export
-# df_export.to_csv('Coordenadas.csv',sep=';',index=False)
-
-# Base de datos 
 df_export.drop(['index'], axis=1)
 df_export['time']=df_export['time'].values.astype('datetime64[s]')
 
-conn = sqlite3.connect('base_general.db')
+## Base de datos
+
+conn = sqlite3.connect('dataset.db')
+
+## Datos crudos
+
 conn.execute('''CREATE TABLE datos_crudos (
+              time TEXT, 
+              latitude FLOAT, 
+              longitude FLOAT, 
+              elevation FLOAT)''')
+
+for index, row in dftc.iterrows():
+    query = "INSERT INTO datos_crudos (level_0, time, latitude, longitude, elevation, Round_lat, Round_long, barrio, city, state, country ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    conn.execute(query, (
+                        row['level_0'], 
+                        row['time'].strftime('%Y-%m-%d %H:%M:%S'),
+                        row['latitude'], 
+                        row['longitude'], 
+                        row['elevation']))
+
+## Datos calculados
+
+conn.execute('''CREATE TABLE datos_calculados (
               level_0 INT,
               time TEXT, 
               latitude FLOAT, 
@@ -102,14 +114,10 @@ conn.execute('''CREATE TABLE datos_crudos (
               barrio TEXT,
               city TEXT,
               state TEXT,
-              country TEXT
-
-)''')
-
-# level_0;index;time;latitude;longitude;elevation;Round_lat;Round_long;barrio;city;state;country
+              country TEXT)''')
 
 for index, row in df_export.iterrows():
-    query = "INSERT INTO datos_crudos (level_0, time, latitude, longitude, elevation, Round_lat, Round_long, barrio, city, state, country ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    query = "INSERT INTO datos_calculados (level_0, time, latitude, longitude, elevation, Round_lat, Round_long, barrio, city, state, country ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
     conn.execute(query, (
                         row['level_0'], 
                         row['time'].strftime('%Y-%m-%d %H:%M:%S'),
